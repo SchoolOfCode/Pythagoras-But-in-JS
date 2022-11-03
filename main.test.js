2a5059c8b9d6cfa6cb4569a7286bb814d3f78892
coffeebeans001 <marcel.maynard001@yahoo.com>
2022-11-03 14:33:22 +0000

    my first eritten test, Robin has done plenty,my first one is amazing

--- main.test.js
+++ main.test.js
@@ -1,1 +1,13 @@
+import { test, expect } from "@jest/globals";
 //👉 Write your tests below here:
+// write a test
+// a test that takes in "a" and "b" and checks that "c" is equal Pythagoras
+// (expected) variable and (actual) variable
+
+import { pythageros } from "./main.js";
+test("test that takes in a and b and checks that c is equal Pythagoras", () => {
+  const actual = pythagoras(3, 4);
+  const expected = 5;
+
+  expected(actual).toEqual(expected);
+});
